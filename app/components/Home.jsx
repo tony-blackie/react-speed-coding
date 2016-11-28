d5ae6ef8fc69eee89cf5ee6af802ec7ee576b162
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-11-28 19:55:22 +0100

    Add graph components to Home component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react';
+import { series, singleSeries, cloudSeries } from '../fixtures/charts';
 import World from './World';
 import Card from './Card';
 import YouTube from './YouTube';
@@ -9,8 +10,36 @@ import ButtonDemo from './ButtonDemo';
 import Input from './Input';
 import InputLabel from './InputLabel';
 import InputField from './InputField';
+const {
+    // main component
+    Chart,
+    // graphs
+    Bars, Cloud, Labels, Lines, Pies, RadialLines, Ticks,
+    // wrappers
+    Layer, Animate, Transform
+} = require('rumble-charts');
+
 
 export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            series, cloudSeries, singleSeries, hovered: {}
+        };
+        this.updateSeries = this.updateSeries.bind(this);
+    }
+    updateSeries = () => {
+        const singleSeries = [
+            {
+                data: _.map(_.range(3), () => Math.random() * 100)
+            }
+        ];
+        const series = _.map(_.range(3), () => ({
+            data: _.map(_.range(3), () => Math.random() * 100)
+        }));
+        this.setState({ series, cloudSeries, singleSeries, hovered: {}});
+    };
+
     render() {
         return(
             <section className="landing">
@@ -144,6 +173,156 @@ export default class Home extends Component {
                             <InputField placeholder="Send a note" />
                             <Button color="warning" label="Send" />
                         </Input>
+                    </Card>
+                </section>
+                <section className="stripe back--default">
+                    <Card className="back--white">
+                        <Chart onClick={this.updateSeries} width={300} height={200} series={this.state.series}>
+                            <Transform method={['transpose', 'stackNormalized']}>
+                                <Pies
+                                    colors="category10"
+                                    combined
+                                    innerRadius="33%"
+                                    padAngle={0.025}
+                                    cornerRadius={5}
+                                    innerPadding={2}
+                                    pieAttributes={{
+                                    onMouseMove: (e) => e.target.style.opacity = 1,
+                                    onMouseLeave: (e) => e.target.style.opacity = 0.5
+                                    }}
+                                    pieStyle={{ opacity: 0.5 }}/>
+                            </Transform>
+                        </Chart>
+                    </Card>
+                    <Card className="back--white">
+                        <Chart onClick={this.updateSeries} width={300} height={200} series={this.state.series} minY={0}>
+                            <Animate _ease="bounce" _ ease="elastic">
+                                <Layer width="80%" height="80%" position="middle center">
+                                    <Ticks
+                                        axis="y"
+                                        ticks={{ maxTicks: 4 }}
+                                        tickVisible={({ tick }) => tick.y > 0}
+                                        lineLength="100%"
+                                        lineVisible
+                                        lineStyle={{ stroke: 'lightgray' }}
+                                        labelStyle={{
+                                            textAnchor: 'end',
+                                            alignmentBaseline: 'middle',
+                                            fontSize: '0.5em',
+                                            fill: 'lightgray'
+                                        }}
+                                        labelAttributes={{ x: -5 }}
+                                        />
+                                    <Ticks
+                                        axis="x"
+                                        label={({ tick }) => tick.x + 1}
+                                        labelStyle={{
+                                            textAnchor: 'middle',
+                                            alignmentBaseline: 'before-edge',
+                                            fontSize: '0.5em',
+                                            fill: 'lightgray'
+                                        }}
+                                        labelAttributes={{ y: 3 }}
+                                        />
+                                    <Bars
+                                        groupPadding="3%"
+                                        innerPadding="0.5%"
+                                        barAttributes={{
+                                            onMouseMove: e => e.target.style.fillOpacity = 1,
+                                            onMouseLeave: e => e.target.style.fillOpacity = 0.5
+                                        }}
+                                        barStyle={{
+                                            fillOpacity: 0.5
+                                        }}
+                                    />
+                                    <Lines lineWidth={2} />
+                                    <Labels
+                                        label={({ point }) => (`y=${Math.round(point.y)}`)}
+                                        dotStyle={{
+                                            textAnchor: 'middle',
+                                            dominantBaseline: 'text-after-edge',
+                                            fontFamily: 'sans-serif',
+                                            fontSize: '0.65em'
+                                        }}
+                                        labelAttributes={{
+                                            y: -4
+                                        }}
+                                    />
+                                </Layer>
+                            </Animate>
+                        </Chart>
+                    </Card>
+                    <Card className="back--white">
+                        <Chart
+                            onClick={this.updateSeries}
+                            width={300}
+                            height={200}
+                            series={this.state.series}
+                            minY={0}
+                        >
+                            <RadialLines />
+                        </Chart>
+                    </Card>
+                </section>
+                <section className="stripe">
+                    <Card>
+                        <Chart
+                            onClick={this.updateSeries}
+                            width={300}
+                            height={200}
+                            series={this.state.singleSeries}
+                            minY={0}
+                        >
+                            <Transform method={['transpose']}>
+                                <Layer width="80%" height="80%">
+                                    <Bars />
+                                </Layer>
+                                <Layer width="25%" height="25%" position="right bottom">
+                                    <Transform method="stack">
+                                        <Pies
+                                            combined
+                                            colors="category10"
+                                            pieStyle={{ opacity: 0.8 }}
+                                        />
+                                    </Transform>
+                                </Layer>
+                            </Transform>
+                        </Chart>
+                    </Card>
+                    <Card>
+                        <Chart
+                            onClick={this.updateSeries}
+                            width={300}
+                            height={200}
+                            series={this.state.series}
+                            minY={0}
+                        >
+                            <Transform method={['transpose', 'stack']}>
+                                <Pies
+                                    combined
+                                    innerPadding="3%"
+                                    innerRadius="20%"
+                                />
+                            </Transform>
+                        </Chart>
+                    </Card>
+                    <Card>
+                        <Chart
+                            onClick={this.updateSeries}
+                            width={300}
+                            height={200}
+                            series={this.state.cloudSeries}
+                            minY={0}
+                        >
+                            <Transform method="transpose">
+                                <Cloud
+                                    font="Arial"
+                                    minFontSize={12}
+                                    maxFontSize={36}
+                                    padding={2}
+                                />
+                            </Transform>
+                        </Chart>
                     </Card>
                 </section>
                 <section className="stripe--slim back--gray">
