a3c6a469572cbfb8bed2905bf43177345cd7500c
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-12-01 18:21:47 +0100

    Use svg icons

---
+++ app/fixtures/icons.js
@@ -0,0 +1,6 @@
+const ICONS = {
+    ARROW_RIGHT: 'M992 512l-480-480v288h-512v384h512v288z',
+    ITALIC: 'M896 64v64h-128l-320 768h128v64h-448v-64h128l320-768h-128v-64z'
+};
+
+export default ICONS;
