f47ad28623c2e1ca3cae9d74b08398d898df2d1f
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-12-12 18:04:30 +0100

    Add test for initial state

--- test/02_components/02_roadmap_redux.spec.js
+++ test/02_components/02_roadmap_redux.spec.js
@@ -1,7 +1,14 @@
 import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import store from '../../app/store/roadmap';
+import * as actions from '../../app/actions/roadmap';
 
 describe('Roadmap Redux Spec', () => {
-    it('should get initial state for store');
+    it('should get initial state for store', () => {
+        expect(store.getState().features.length).to.equal(0);
+        expect(store.getState().categoryFilter).to.equal(actions.CategoryFilters.SHOW_ALL);
+        expect(store.getState().searchText).to.equal('');
+    });
     it('should add first feature of Component category');
     it('should initialize first feature with default state');
     it('should increment likes count for first feature');
