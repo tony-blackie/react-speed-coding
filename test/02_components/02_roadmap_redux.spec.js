1ff8c3aa80891ad5270ad8bbda3eb982c40beeb5
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-12-12 18:30:54 +0100

    Add tests for categoryFilter function

--- test/02_components/02_roadmap_redux.spec.js
+++ test/02_components/02_roadmap_redux.spec.js
@@ -9,10 +9,25 @@ describe('Roadmap Redux Spec', () => {
         expect(store.getState().categoryFilter).to.equal(actions.CategoryFilters.SHOW_ALL);
         expect(store.getState().searchText).to.equal('');
     });
-    it('should add first feature of Component category');
-    it('should initialize first feature with default state');
-    it('should increment likes count for first feature');
-    it('should set a new categoryFilter');
+    it('should add first feature of Component category', () => {
+        store.dispatch(actions.addFeature('New Component Feature', actions.Categories.COMPONENT));
+
+        expect(store.getState().features.length).to.equal(1);
+        expect(store.getState().features[0].category).to.equal(actions.Categories.COMPONENT)
+    });
+    it('should initialize first feature with default state', () => {
+        expect(store.getState().features[0].likes).to.equal(0);
+    });
+    it('should increment likes count for first feature', () => {
+        store.dispatch(actions.likeFeature(0));
+        store.dispatch(actions.likeFeature(0));
+        expect(store.getState().features[0].likes).to.equal(2);
+    });
+    it('should set a new categoryFilter', () => {
+        expect(store.getState().categoryFilter).to.equal(actions.CategoryFilters.SHOW_ALL);
+        store.dispatch(actions.setCategoryFilter(actions.CategoryFilters.SHOW_COMPONENTS));
+        expect(store.getState().categoryFilter).to.equal(actions.CategoryFilters.SHOW_COMPONENTS);
+    });
     it('should add second feature of CHAPTER category');
     it('should add third feature of APP category');
     it('should set new search text');
